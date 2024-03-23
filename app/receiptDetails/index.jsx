1c73eef3ee0d0958627eecea035007760dad8ca0
Eduardo Santos <52334473+SkDudu@users.noreply.github.com>
2024-03-23 20:03:04 -0300

    finish add dialog and actionsheet

--- app/receiptDetails/index.jsx
+++ app/receiptDetails/index.jsx
@@ -1,11 +1,18 @@
+import React from 'react';
 import { Link } from 'expo-router';
-import { EllipsisVertical, Music4, Percent, PlusCircle } from 'lucide-react-native';
+import { EllipsisVertical, Music4, Pencil, Percent, PlusCircle, ReceiptIcon, ReceiptText, SquareArrowOutUpRight, SquareX } from 'lucide-react-native';
 
-import { Button, HStack, Icon, IconButton, StatusBar, Text, VStack, View } from 'native-base';
+import { Actionsheet, AlertDialog, Button, HStack, Icon, IconButton, Pressable, StatusBar, Text, VStack, View, useDisclose } from 'native-base';
 
 import CardParticipant from '../../app/components/cardParticipant';
 
 export default function index() {
+  const {isOpen, onOpen, onClose} = useDisclose();
+  
+  const [isOpenDialog, setIsOpen] = React.useState(false);
+  const onCloseDialog = () => setIsOpen(false);
+  const cancelRef = React.useRef(null);
+
   return (
     <>
       <StatusBar barStyle={'dark-content'} />
@@ -16,7 +23,7 @@ export default function index() {
               <Text fontSize={"22px"} fontWeight={'medium'} color={"white"}>Conta do praiow</Text>
               <Text fontSize={"14"} fontWeight={'normal'} color={"white"}>Responsável: Bágri</Text>
             </VStack>
-            <IconButton icon={<Icon as={<EllipsisVertical color={"white"} size={18}/>} />}/>
+            <IconButton onPress={onOpen} icon={<Icon as={<EllipsisVertical color={"white"} size={18}/>} />}/>
           </HStack>
           <VStack alignItems={'center'} mt={6}>
             <Text fontSize={"12"} fontWeight={'normal'} color={"white"}>Seu consumo total</Text>
@@ -67,6 +74,55 @@ export default function index() {
           <CardParticipant />
         </View>
       </VStack>
+      <Actionsheet isOpen={isOpen} onClose={onClose}>
+        <Actionsheet.Content>
+          <Actionsheet.Item>
+            <HStack alignItems={'center'} space={2}>
+              <Pencil size={18} color={"black"}/>
+              <Text fontSize={"16px"} fontWeight={"normal"}>Editar informações da conta</Text>
+            </HStack>
+          </Actionsheet.Item>
+          <Actionsheet.Item>
+            <HStack alignItems={'center'} space={2}>
+              <SquareArrowOutUpRight size={18} color={"black"}/>
+              <Text fontSize={"16px"} fontWeight={"normal"}>Compartilhar conta</Text>
+            </HStack>
+          </Actionsheet.Item>
+          <Actionsheet.Item>
+            <HStack alignItems={'center'} space={2}>
+              <ReceiptText size={18} color={"black"}/>
+              <Text fontSize={"16px"} fontWeight={"normal"}>Resumo da sua conta</Text>
+            </HStack>
+          </Actionsheet.Item>
+          <Actionsheet.Item>
+            <Pressable onPress={() => setIsOpen(!isOpenDialog)}>
+              <HStack alignItems={'center'} space={2}>
+                <SquareX size={18} color={"#FF0000"}/>
+                <Text fontSize={"16px"} fontWeight={"normal"} color={"#FF0000"}>Encerrar a conta compartilhada</Text>
+              </HStack>
+            </Pressable>
+          </Actionsheet.Item>
+        </Actionsheet.Content>
+      </Actionsheet>
+      <AlertDialog leastDestructiveRef={cancelRef} isOpen={isOpenDialog} onClose={onCloseDialog}>
+        <AlertDialog.Content>
+          <AlertDialog.CloseButton />
+          <AlertDialog.Header>Encerrar conta compartilhada?</AlertDialog.Header>
+          <AlertDialog.Body>
+            Você deseja encerrar essa conta compartilhada?
+          </AlertDialog.Body>
+          <AlertDialog.Footer>
+            <Button.Group space={2}>
+              <Button variant="unstyled" colorScheme="coolGray" onPress={onCloseDialog} ref={cancelRef}>
+                Não
+              </Button>
+              <Button bgColor={"black"} onPress={onCloseDialog}>
+                Sim
+              </Button>
+            </Button.Group>
+          </AlertDialog.Footer>
+        </AlertDialog.Content>
+      </AlertDialog>
     </>
   )
 }
