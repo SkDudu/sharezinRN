62c1f03443f914ebdfda7b55a73b940d48424148
Eduardo Santos <52334473+SkDudu@users.noreply.github.com>
2024-03-23 20:02:42 -0300

    update name in home/index

--- app/(tabs)/home/index.jsx
+++ app/(tabs)/home/index.jsx
@@ -3,7 +3,7 @@ import { Plus, PlusCircle } from 'lucide-react-native';
 import { Button, Fab, HStack, Icon, StatusBar, Text, VStack } from 'native-base';
 import CardReceipt from '../../components/cardReceipt'
 
-export default function searchReceipt() {
+export default function Home() {
   return (
     <>
       <StatusBar barStyle={'dark-content'} />
