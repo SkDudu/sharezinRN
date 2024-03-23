8670ea1f5dd24733c9f4fbb80c22534a2a867902
Eduardo Santos <52334473+SkDudu@users.noreply.github.com>
2024-03-23 10:14:35 -0300

    update in route and index for home

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,15 +1,18 @@
 import { Stack } from 'expo-router';
+import { NativeBaseProvider } from 'native-base';
 
 export default function Layout() {
   return (
-    <Stack screenOptions={{headerShown: false}}>
-      <Stack.Screen 
-        name="index"
-        options={{
-          title: 'Sign In',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
-        }}
-      />
-    </Stack>
+    <NativeBaseProvider>
+      <Stack screenOptions={{headerShown: false}}>
+        <Stack.Screen 
+          name="index"
+          options={{
+            title: 'Sign In',
+            tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+          }}
+        />
+      </Stack>
+    </NativeBaseProvider>
   )
 }
--- app/index.jsx
+++ app/index.jsx
@@ -8,39 +8,39 @@ import logo from "../assets/logo.png"
 
 export default function Page() {
   return (
-    <NativeBaseProvider>
-      <StatusBar style="light" />
-      <ZStack width={"full"} height={"full"}>
-        <Image 
-          alt="pattern of icons"
-          source={bg}
-          width={"full"}
-          height={"full"}
-        />
-        <VStack width={"full"} height={"full"} paddingX={"10px"} paddingY={"30px"} justifyContent={ 'space-between'}>
-          <VStack space={"70px"}>
-            <VStack alignItems={'center'} pt={"150px"}>
-              <Image 
-                alt="pattern of icons"
-                source={logo}
-                width={"50px"}
-                height={"60px"}
-                resizeMode='contain'
-              />
-              <Text alignSelf={"center"} fontSize={'32px'} fontWeight={'semibold'} color={"white"}>Sharezin</Text>
+      <>
+        <StatusBar style="light" />
+        <ZStack width={"full"} height={"full"}>
+          <Image 
+            alt="pattern of icons"
+            source={bg}
+            width={"full"}
+            height={"full"}
+          />
+          <VStack width={"full"} height={"full"} paddingX={"10px"} paddingY={"30px"} justifyContent={ 'space-between'}>
+            <VStack space={"70px"}>
+              <VStack alignItems={'center'} pt={"150px"}>
+                <Image 
+                  alt="pattern of icons"
+                  source={logo}
+                  width={"50px"}
+                  height={"60px"}
+                  resizeMode='contain'
+                />
+                <Text alignSelf={"center"} fontSize={'32px'} fontWeight={'semibold'} color={"white"}>Sharezin</Text>
+              </VStack>
+              <VStack>
+                <Text alignSelf={"center"} fontSize={'lg'} fontWeight={'semibold'} color={"white"}>Seja inteligente!</Text>
+                <Text alignSelf={"center"} fontSize={'md'} fontWeight={'normal'} color={"#8c8c8c"}>Uma maneira de controlar a conta da saideira.</Text>
+              </VStack>
             </VStack>
-            <VStack>
-              <Text alignSelf={"center"} fontSize={'lg'} fontWeight={'semibold'} color={"white"}>Seja inteligente!</Text>
-              <Text alignSelf={"center"} fontSize={'md'} fontWeight={'normal'} color={"#8c8c8c"}>Uma maneira de controlar a conta da saideira.</Text>
-            </VStack>
+            <Link replace href="/signIn" asChild>
+              <Button width={"full"} height={"56px"} alignItems={"center"} justifyContent={'center'} bgColor={"white"} rounded={"md"}>
+                  <Text alignSelf={"center"} fontSize={'md'} fontWeight={'semibold'}>Vamos começar</Text>
+              </Button>
+            </Link>
           </VStack>
-          <Link replace href="/signIn" asChild>
-            <Button width={"full"} height={"56px"} alignItems={"center"} justifyContent={'center'} bgColor={"white"} rounded={"md"}>
-                <Text alignSelf={"center"} fontSize={'md'} fontWeight={'normal'}>Vamos começar</Text>
-            </Button>
-          </Link>
-        </VStack>
-      </ZStack>
-    </NativeBaseProvider>
+        </ZStack>
+      </>
   )
 }
